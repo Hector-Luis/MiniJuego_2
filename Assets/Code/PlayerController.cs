a59e5e54556c2ba9cbac06686da8ba8866d6c958
Hector-Luis <hluischimbo@gmail.com>
2019-06-28 20:11:08 -0500

    movimiento horizontal

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 2f;
+    public float max_speed = 5f;
+    public bool toca_suelo;
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("velocidad", Mathf.Abs(rb2d.velocity.x));
+        animator.SetBool("toca_suelo", toca_suelo);
+    }
+
+    void FixedUpdate() {
+        float h = Input.GetAxis("Horizontal");
+        rb2d.AddForce(Vector2.right * speed * h);
+        /*if (rb2d.velocity.x > max_speed) {
+            rb2d.velocity = new Vector2(max_speed, rb2d.velocity.y);
+        }
+        if (rb2d.velocity.x < -max_speed)
+        {
+            rb2d.velocity = new Vector2(-max_speed, rb2d.velocity.y);
+        }*/
+
+        float limit_speed = Mathf.Clamp(rb2d.velocity.x, -max_speed, max_speed);
+        rb2d.velocity = new Vector2(limit_speed, rb2d.velocity.y);
+
+        Debug.Log(toca_suelo);
+    }
+}
---
+++ Assets/Code/TocaSuelo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TocaSuelo : MonoBehaviour
+{
+    private PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionStay2D(Collision2D col){
+        player.toca_suelo = true;
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        player.toca_suelo = false;
+    }
+}
